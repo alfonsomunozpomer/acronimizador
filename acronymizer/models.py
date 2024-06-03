bd5bce04e0c703c19ad292c7c2b0009d3c87c1e7
Alfonso Muñoz-Pomer Fuentes <alfonso.munozpomer@proton.me>
2024-06-03 13:55:38 +0100

    Add string serialization of models and utility methods

--- acronymizer/models.py
+++ acronymizer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # The longest word in Spanish is "Electroencefalografista" with 23 characters
 class Submission(models.Model):
@@ -9,9 +10,20 @@ class Submission(models.Model):
     }
 
     word = models.CharField(max_length=25)
-    sent_date = models.DateTimeField()
+    sent_date = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=1, choices=status_choices, default='P') 
     updated_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.word \
+               + ' (' + self.sent_date.strftime('%Y-%m-%d %H:%M:%S') + '), ' \
+               + 'status: [' + self.status + ']' \
+               + (' ' + self.updated_date.strftime('%Y-%m-%d %H:%M:%S') if self.updated_date else '') \
+               + ' ' + str(self.acronymtoken_set.all())
+    
+    def perfect(self):
+        return all(token.perfect() for token in self.acronymtoken_set.all())
+
 
 class AcronymToken(models.Model):
     prepositions = {'a', 'ante', 'bajo', 'cabe', 'con', 'contra', 'de', 'desde', 'durante', 'en', 'entre', 'hacia', 'hasta', 'mediante', 'para', 'por', 'según', 'sin', 'so', 'sobre', 'tras'}
@@ -22,5 +34,15 @@ class AcronymToken(models.Model):
     # Prepositions *may* optionally be part of the acronym
     preposition = models.CharField(max_length=8, choices=preposition_choices, null=True)
     order = models.IntegerField()
+
+    def perfect(self):
+        return self.preposition is None
+
+    def __str__(self):
+        return self.token \
+               + (' ' + self.preposition if self.preposition else '') \
+               + ' [' + str(self.order) + ']' \
+               + ' (' + self.submission.word + ')'
+               
 
 
