a77778122b87fb35aa3f2b1daea263573216757a
Alfonso Muñoz-Pomer Fuentes <alfonso.munozpomer@proton.me>
2024-06-05 19:08:59 +0100

    Flesh out list and detail views

--- acronymizer/views.py
+++ acronymizer/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Submission
 
 def index(request):
-    return HttpResponse("EL ACRONIMIZADOR")
+    latest_definitions = Submission.objects.filter(status='A').order_by('-sent_date')[:5]
+    context = {
+        'latest_definitions_list': latest_definitions,
+    }
+    return render(request, 'acronymizer/index.html', context)
 
 def definition(request, word):
-    return HttpResponse(f'Definición de {word}')
+    definition_list = get_list_or_404(Submission, word=word, status='A')
+    context = {
+        'word': word,
+        'definition_list': definition_list
+    }
+    return render(request, 'acronymizer/detail.html', context)
 
 
 def define(request, word):
